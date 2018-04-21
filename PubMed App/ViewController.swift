9db289ced045891293c6d00cd8dff3d2a7fd74f5
salmouaswas <salmouaswas@deloitte.ca>
2018-04-21 14:10:49 -0400

    Initial Commit

---
+++ PubMed App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PubMed App
+//
+//  Created by Al Mouaswas, Sara (CA - Toronto) on 2018-04-21.
+//  Copyright © 2018 Al Mouaswas, Sara (CA - Toronto). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PubMed AppTests/PubMed_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  PubMed_AppTests.swift
+//  PubMed AppTests
+//
+//  Created by Al Mouaswas, Sara (CA - Toronto) on 2018-04-21.
+//  Copyright © 2018 Al Mouaswas, Sara (CA - Toronto). All rights reserved.
+//
+
+import XCTest
+@testable import PubMed_App
+
+class PubMed_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
